6d6ba5c827f11b82a076d8f975dccd16a9eeaf88
NadaAlhabbash <nadaalhabbash@gmail.com>
2024-02-29 19:33:09 +0300

    added the slider

---
+++ src/app/components/toggle-taps/toggle-taps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-taps',
+  templateUrl: './toggle-taps.component.html',
+  styleUrl: './toggle-taps.component.css'
+})
+export class ToggleTapsComponent {
+
+}
