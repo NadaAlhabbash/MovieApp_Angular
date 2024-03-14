1b39383edc32d23f88636f4a87b94e50bfa99d3e
NadaAlhabbash <101626858+NadaAlhabbash@users.noreply.github.com>
2024-03-15 00:10:24 +0300

    Revert "adding the services"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { DetailsComponent } from './components/details/details.component';
-import { WatchlistComponent } from './components/watchlist/watchlist.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
-  {path: 'details', component: DetailsComponent},
-  {path: 'watchlist', component: WatchlistComponent}
+  {path: 'details', component: DetailsComponent}
 ];
 
 @NgModule({
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -8,40 +8,40 @@ import { Movie } from '../../models/movie';
 })
 export class SearchComponent {
 
-  // movies: Movie[] = [
-  //   { 
-  //     img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
-  //     title: 'Movie Title',
-  //     rating: 4.5,
-  //     type: 'Action',
-  //     year: 2022,
-  //     duration: '2h 30min'
-  //   },
-  //   { 
-  //     img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
-  //     title: 'Movie Title',
-  //     rating: 4.5,
-  //     type: 'Action',
-  //     year: 2022,
-  //     duration: '2h 30min'
-  //   },
-  //   { 
-  //     img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
-  //     title: 'Movie Title',
-  //     rating: 4.5,
-  //     type: 'Action',
-  //     year: 2022,
-  //     duration: '2h 30min'
-  //   },
-  //   { 
-  //     img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
-  //     title: 'Movie Title',
-  //     rating: 4.5,
-  //     type: 'Action',
-  //     year: 2022,
-  //     duration: '2h 30min'
-  //   },
+  movies: Movie[] = [
+    { 
+      img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
+      title: 'Movie Title',
+      rating: 4.5,
+      type: 'Action',
+      year: 2022,
+      duration: '2h 30min'
+    },
+    { 
+      img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
+      title: 'Movie Title',
+      rating: 4.5,
+      type: 'Action',
+      year: 2022,
+      duration: '2h 30min'
+    },
+    { 
+      img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
+      title: 'Movie Title',
+      rating: 4.5,
+      type: 'Action',
+      year: 2022,
+      duration: '2h 30min'
+    },
+    { 
+      img: 'https://assets.mubicdn.net/images/notebook/post_images/31915/images-w1400.jpeg?1607880449',
+      title: 'Movie Title',
+      rating: 4.5,
+      type: 'Action',
+      year: 2022,
+      duration: '2h 30min'
+    },
     
-  // ];
+  ];
   
 }
